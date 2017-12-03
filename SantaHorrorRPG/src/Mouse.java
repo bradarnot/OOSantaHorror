e5b3296e27938a2cd1ba5322635d2039430e3ce7
Kelsey Dowd <kedo8480@colorado.edu>
2017-12-03 12:11:45 -0700

    Updated mouse class

--- SantaHorrorRPG/src/Mouse.java
+++ SantaHorrorRPG/src/Mouse.java
@@ -1,4 +1,22 @@
 
 public class Mouse {
-
+	public void mouseClicked(e MouseEvent) {
+		
+	}
+	
+	public void mouseEntered(e MouseEvent) {
+		
+	}
+	
+	public void mouseExited(e MouseEvent) {
+		
+	}
+	
+	public void mousePressed(e MouseEvent) {
+		
+	}
+	
+	public void mouseReleased(e MouseEvent) {
+		
+	}
 }

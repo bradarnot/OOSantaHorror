25ee0c4d2a6643fc3ee7b17567a33ed70b45c39a
Kelsey Dowd <kedo8480@colorado.edu>
2017-12-03 12:53:25 -0700

    Updated class Input

--- SantaHorrorRPG/src/Input.java
+++ SantaHorrorRPG/src/Input.java
@@ -1,4 +1,21 @@
 
 public class Input {
-
+	private ArrayList<String> inputs;
+	private Position location;
+	
+	public ArrayList<String> getInputs() {
+		return inputs;
+	}
+	
+	public void setInputs(ArrayList<String> al) {
+		
+	}
+	
+	public Position getLocation() {
+		return location;
+	}
+	
+	public void setLocation(Position pos) {
+		
+	}
 }

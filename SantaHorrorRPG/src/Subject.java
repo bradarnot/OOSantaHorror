7c3efec8e082c7f57d43349ada60a4a044b87a32
Kelsey Dowd <kedo8480@colorado.edu>
2017-12-03 12:45:23 -0700

    Updated Subject Class

--- SantaHorrorRPG/src/Subject.java
+++ SantaHorrorRPG/src/Subject.java
@@ -1,4 +1,13 @@
-
 public class Subject {
+	protected ArrayList<Observer> observers;
 
+	public abstract void notify(Input input);
+	
+	public void attach(Observer o) {
+		
+	}
+	
+	public void dettach(Observer o) {
+		
+	}
 }

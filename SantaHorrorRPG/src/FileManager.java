a4660f650fe0f93b48af2a0721a005ef0691222b
jeffreylipnick <lipnick@colorado.edu>
2017-12-04 13:39:03 -0700

    Setup basic database connection in loadZone();

--- SantaHorrorRPG/src/FileManager.java
+++ SantaHorrorRPG/src/FileManager.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.json.simple.parser.JSONParser;
 
 public class FileManager {
@@ -5,7 +9,18 @@ public class FileManager {
 	private JSONParser parser;
 	
 	public void loadZone(String name) {
-		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/santa_horror","santa", "password");
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void loadGameObj(String name) {

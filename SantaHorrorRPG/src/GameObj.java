1b6c9d061c47e7b0ebad4d0cd99817ed987484c1
Nicholas Pfeufer <nipf1078@colorado.edu>
2017-12-04 13:44:34 -0700

    gameObject interaction interaction

--- SantaHorrorRPG/src/GameObj.java
+++ SantaHorrorRPG/src/GameObj.java
@@ -17,6 +17,15 @@ public class GameObj {
 	protected ArrayList<Interaction> playerInteractions;
 	protected String swapInTo;
 	protected Image appearance;
+	
+	public Image getAppearance() {
+		return appearance;
+	}
+
+	public void setAppearance(Image appearance) {
+		this.appearance = appearance;
+	}
+
 	protected boolean solid;
 	protected Position position;
 	
@@ -27,15 +36,17 @@ public class GameObj {
 	public Interaction interact(boolean byPlayer, String direction) {
 		Interaction result = nonPlayerInteractions.get(nonPlayerInteractState);
 		if(byPlayer) {
-			
-		}
-		
+			result = playerInteractions.get(playerInteractState);
+			playerInteractState = result.getNextInteraction(direction);
+		}else {
+			nonPlayerInteractState = result.getNextInteraction(direction);
+		}		
 		return result;
 		
 	}
 	
 	public void swap() {
-		
+		this.loadFromFile(swapInTo, position);		
 	}
 	
 	public boolean isObject(String name) {

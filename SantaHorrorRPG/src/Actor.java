d0baba18bf521ab53862348c433ade976ff8dcd6
Nicholas Pfeufer <nipf1078@colorado.edu>
2017-12-05 23:59:24 -0700

    Abstract damage
    Actor fixes

--- SantaHorrorRPG/src/Actor.java
+++ SantaHorrorRPG/src/Actor.java
@@ -1,4 +1,4 @@
-public class Actor extends GameObj {
+public abstract class Actor extends GameObj {
 	protected int speed;
 	protected int currentTic;
 	protected int imageFrame;
@@ -9,8 +9,10 @@ public class Actor extends GameObj {
 		
 	}
 	
+	//If it dies it returns false
 	public boolean damage(int damage) {
-		return true;
+		this.health -= damage;
+		return this.health <= 0;
 	}
 	
 	public void move() {

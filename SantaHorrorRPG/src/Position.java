18614b6e0082560c8851759af9d54ddb3b9d744b
Nicholas Pfeufer <nipf1078@colorado.edu>
2017-12-06 00:26:19 -0700

    Fixed Position Setters

--- SantaHorrorRPG/src/Position.java
+++ SantaHorrorRPG/src/Position.java
@@ -4,10 +4,7 @@ public class Position {
 	private int x;
 	private int y;
 	
-	public int getX() {
-		return x;
-		
-	}
+
 	
 	public Position(int x, int y) {
 		super();
@@ -15,17 +12,30 @@ public class Position {
 		this.setY(y);
 	}
 
-	public int getY() {
+
+
+	public int getX() {
 		return x;
-		
 	}
-	
+
+
+
 	public void setX(int x) {
-		
+		this.x = x;
 	}
-	
+
+
+
+	public int getY() {
+		return y;
+	}
+
+
+
 	public void setY(int y) {
-		
+		this.y = y;
 	}
+
+
 	
 }

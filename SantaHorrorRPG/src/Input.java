c0b3dfe51ea683b94e8df8b508843378099a6b22
Kelsey Dowd <kedo8480@colorado.edu>
2017-12-07 14:07:44 -0700

    Added input and subject functionality

--- SantaHorrorRPG/src/Input.java
+++ SantaHorrorRPG/src/Input.java
@@ -1,22 +1,48 @@
-import java.util.ArrayList;
-
 public class Input {
-	private ArrayList<String> inputs;
-	private Position location;
+	private boolean up;
+	private boolean down;
+	private boolean left;
+	private boolean right;
+	private boolean interact; 
 	
-	public ArrayList<String> getInputs() {
-		return inputs;
+	public boolean isInteract() {
+		return interact;
 	}
 	
-	public void setInputs(ArrayList<String> al) {
-		
+	public void setInteract(boolean interact) {
+		this.interact = interact;
 	}
 	
-	public Position getLocation() {
-		return location;
+	public boolean isUp() {
+		return up;
 	}
 	
-	public void setLocation(Position pos) {
-		
+	public void setUp(boolean up) {
+		this.up = up;
 	}
+	
+	public boolean isDown() {
+		return down;
+	}
+	
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+	
+	public boolean isLeft() {
+		return left;
+	}
+	
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+	
+	public boolean isRight() {
+		return right;
+	}
+	
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
 }
--- SantaHorrorRPG/src/Keyboard.java
+++ SantaHorrorRPG/src/Keyboard.java
@@ -1,7 +1,14 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Keyboard implements KeyListener {
+public class Keyboard extends Subject implements KeyListener {
+	private Input input;
+	
+	public Keyboard() {
+		super();
+		this.input = new Input();
+	}
+
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
@@ -13,17 +20,25 @@ public class Keyboard implements KeyListener {
 		// TODO Auto-generated method stub
 		System.out.println("Pressed");
 		switch (e.getKeyCode()) {
+			case KeyEvent.VK_S:
 			case KeyEvent.VK_DOWN:
 				System.out.println("Down");
+				this.input.setDown(true);
 				break;
+			case KeyEvent.VK_W:
 			case KeyEvent.VK_UP:
 				System.out.println("Up");
+				this.input.setUp(true);
 				break;
+			case KeyEvent.VK_A:
 			case KeyEvent.VK_LEFT:
 				System.out.println("Left");
+				this.input.setLeft(true);
 				break;
+			case KeyEvent.VK_D:
 			case KeyEvent.VK_RIGHT:
 				System.out.println("Right");
+				this.input.setRight(true);
 				break;
 		}
 	}
@@ -31,6 +46,36 @@ public class Keyboard implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Released");
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_S:
+			case KeyEvent.VK_DOWN:
+				System.out.println("Down");
+				this.input.setDown(false);
+				break;
+			case KeyEvent.VK_W:
+			case KeyEvent.VK_UP:
+				System.out.println("Up");
+				this.input.setUp(false);
+				break;
+			case KeyEvent.VK_A:
+			case KeyEvent.VK_LEFT:
+				System.out.println("Left");
+				this.input.setLeft(false);
+				break;
+			case KeyEvent.VK_D:
+			case KeyEvent.VK_RIGHT:
+				System.out.println("Right");
+				this.input.setRight(false);
+				break;
+		}
+	}
+
+	@Override
+	public void notify(Input input) {
+		for(int index=0;index<this.observers.size();index++) {
+			this.observers.get(index).update(input);
+		}
 		
 	}
 }

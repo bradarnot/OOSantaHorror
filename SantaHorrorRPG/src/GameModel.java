7ef242e114f8f1f3f524c8df22e5752c65b9f30c
Nicholas Pfeufer <nipf1078@colorado.edu>
2017-12-09 23:49:36 -0700

    Collision maybe fix

--- SantaHorrorRPG/src/GameModel.java
+++ SantaHorrorRPG/src/GameModel.java
@@ -175,7 +175,18 @@ public class GameModel {
 		Position topLeft = new Position(tile.getX() - (tile.getX() % this.getTileSize()),
 				tile.getY() - (tile.getY() % this.getTileSize()));
 		
-		ArrayList<GameObj> objectsTopLeft = this.getObjectsAtPosition(topLeft);
+		Position topRight = new Position(tile.getX(), tile.getY());
+		topRight.setX(tile.getX()+this.getTileSize()-1);
+
+		topRight.setX(tile.getX() - (tile.getX() % this.getTileSize()));
+		topRight.setY(tile.getY() - (tile.getY() % this.getTileSize()));
+		
+		Position botLeft = new Position(tile.getX(), tile.getY());
+		botLeft.setY(tile.getY()+this.getTileSize()-1);
+
+		botLeft.setX(tile.getX() - (tile.getX() % this.getTileSize()));
+		botLeft.setY(tile.getY() - (tile.getY() % this.getTileSize()));
+		
 		
 		tile.setX(tile.getX()+this.getTileSize()-1);
 		tile.setY(tile.getY()+this.getTileSize()-1);
@@ -183,11 +194,27 @@ public class GameModel {
 		tile.setX(tile.getX() - (tile.getX() % this.getTileSize()));
 		tile.setY(tile.getY() - (tile.getY() % this.getTileSize()));
 		
-		
+		ArrayList<GameObj> objectsTopLeft = this.getObjectsAtPosition(topLeft);
+		ArrayList<GameObj> objectsTopRight = this.getObjectsAtPosition(topRight);
+		ArrayList<GameObj> objectsBotLeft = this.getObjectsAtPosition(botLeft);
 		ArrayList<GameObj> objectsBotRight = this.getObjectsAtPosition(tile);
 		
 		for(int index = 0; index < objectsTopLeft.size(); index++) {
 			GameObj object = objectsTopLeft.get(index);
+			if(object != null && object.isSolid() && !object.isObject(ignore)) {
+				return false;
+			}	
+		}
+		
+		for(int index = 0; index < objectsTopRight.size(); index++) {
+			GameObj object = objectsTopRight.get(index);
+			if(object != null && object.isSolid() && !object.isObject(ignore)) {
+				return false;
+			}	
+		}
+		
+		for(int index = 0; index < objectsBotLeft.size(); index++) {
+			GameObj object = objectsBotLeft.get(index);
 			if(object != null && object.isSolid() && !object.isObject(ignore)) {
 				return false;
 			}	

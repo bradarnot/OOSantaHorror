0fa73e4bd889e2a28dbbeb19043c7a610c5560a4
jeffreylipnick <lipnick@colorado.edu>
2017-12-10 18:02:27 -0700

    Removed unused functions in Filemanager

--- SantaHorrorRPG/src/FileManager.java
+++ SantaHorrorRPG/src/FileManager.java
@@ -12,22 +12,6 @@ public class FileManager {
 		return JsonParser.getJson("zones", zoneFile);
 	}
 	
-	public void loadGameObj(String name) {
-		
-	}
-	
-	public void loadMonster(String name) {
-		
-	}
-	
-	public void loadPlayer(String name) {
-		
-	}
-
-	public void loadNPC(String name) {
-	
-	}
-	
 	public static JSONObject loadSaveFile(String player) {
 		DBConnection db = DBConnection.getInstance();
 		Number zoneID = db.getZoneID(player);
@@ -38,11 +22,6 @@ public class FileManager {
 	public static void saveGame(String player, int zoneID) {
 		DBConnection db = DBConnection.getInstance();
 		db.saveGame(player, zoneID);
-	}
-	
-	public static void main(String[] argv) {
-		saveGame("Jeff", 1);
-		System.out.println(loadSaveFile("Jeff"));
 	}
 
 }
